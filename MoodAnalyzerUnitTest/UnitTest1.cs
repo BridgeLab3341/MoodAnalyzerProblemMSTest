19d28659345f592fcf0107b55f932ed6805c33ca
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-07 08:23:56 +0530

    [somu]Add:Refactoring the Code.

--- MoodAnalyzerUnitTest/UnitTest1.cs
+++ MoodAnalyzerUnitTest/UnitTest1.cs
@@ -88,8 +88,8 @@ namespace MoodAnalyzerUnitTest
             try
             {
                 string className = "MoodAnalyzerProblemMSTest.Mood";
-                //string constructorParameter = "InValidParameter";
-                object factory = MoodAnalyzerFactory.CreateMoodAnalyser(className);
+                string constructorParameter = "InValidParameter";
+                object factory = MoodAnalyzerFactory.CreateMoodAnalyzers(className,constructorParameter);
             }
             catch (MoodAnalyzerException ex)
             {
@@ -125,8 +125,8 @@ namespace MoodAnalyzerUnitTest
             try
             {
                 string className = "MoodAnalyzerProblemMSTest.Mood";
-                //string constructor = "InValidParameter";
-                object factory = MoodAnalyzerFactory.CreateMoodAnalyser(className);
+                string constructor = "InValidParameter";
+                object factory = MoodAnalyzerFactory.CreateMoodAnalyzers(className,constructor);
             }
             catch (MoodAnalyzerException ex)
             {
